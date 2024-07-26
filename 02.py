a863ac4501d8f39c4fedb85808b23689f60d9d17
Matheus Araujo <167690518+MatheusFAraujo@users.noreply.github.com>
2024-07-27 01:07:12 +0700

    Add files via upload

---
+++ 02.py
@@ -0,0 +1,10 @@
+# Como verificar se o número é Par ou Ímpar?
+
+numero = float(input('Dígite um número: '))
+if numero % 2 == 0:
+    print('Esse número é Par!')
+else:
+    print('Esse número é Impar!')
+
+# Pegando um número qualquer e vendo se o resto da divisão é igual a 0,
+# caso seja, então ele é um número Par, caso contrário, ele é um número Ímpar.
